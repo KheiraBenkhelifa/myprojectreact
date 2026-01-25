763025c1ab02d75f66d6f16e36044671fa0a662d
Kheira Benkhelifa <k.madureira4182@gmail.com>
2026-01-25 14:21:31 +0000

    Update project name on config files and Readme.md

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/myprojectreact/",
+  base: "/code-way-academy/",
   plugins: [react()],
 })
